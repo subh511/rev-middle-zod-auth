eba19a371c3252c53d77f2c59e50e1c50c426513
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-04-03 11:22:15 +0530

    posting an login

--- zodschema.js
+++ zodschema.js
@@ -1,31 +1,47 @@
 const express = require("express");
 const zod = require("zod");
 const app = express();
+app.use(express.json())
 //const port = 3000;
 
+// function validateInput(arr){
 
-function validateInput(arr){
+//     const schema = zod.array(zod.number());
+//     const response = schema.safeParse(arr)
+//     console.log(response)
+// }
 
-    const schema = zod.array(zod.number());
-    const response = schema.safeParse(arr)
-    console.log(response)
-}
+function validateObject(obj) {
+  const schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(6),
+  });
 
-function validateObject(obj){
-    const schema = zod.object({
-        email: zod.string().email(),
-        password: zod.string().min(6)
-    })
-
-    const response = schema.safeParse(obj)
-    console.log(response)
+  const response = schema.safeParse(obj);
+  console.log(response);
 }
 
 // validateInput([1,'2',3,4])
- validateObject({
-    email:"samrat@gmail.com",
-    password:"123456"
- })
+//  validateObject({
+//     email:"samrat@gmail.com",
+//     password:"samrat"
+//  })
 
-app.listen(3000)
+app.post("/login", function (req, res) {
+  const inputs = req.body.input;
+  const response = validateObject(inputs);
+  if (!response.success) {
+    res.json({
+        msg:"your inputs are incorrect"
+    });
+    return;
+  }
+  
+//   res.json({
+//     msg: "your inputs are valid",
+//   });
 
+});
+
+
+app.listen(3000);
